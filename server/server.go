87b43eca7848b71913b62f9d025fc81353052727
@linhdv <linhdv@hblab.vn>
2022-09-09 16:41:11 +0700

    first commit

---
+++ server/server.go
@@ -0,0 +1,14 @@
+package server
+
+import "flag"
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 50051, "The server port")
+)
+
+type routeGuideServer struct {
+}
